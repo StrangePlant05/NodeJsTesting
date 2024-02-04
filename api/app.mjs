4496749f7bbb59ed4209d2394e9cb2ee9c44ca59
StrangePlant05 <iamzeth05@gmail.com>
2024-02-04 17:24:12 +0800

    asdasd

--- api/app.mjs
+++ api/app.mjs
@@ -16,7 +16,7 @@ const __dirname = dirname(__filename);
 app.use(express.static(__dirname))
 
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html');
+    res.sendFile(__dirname + '../index.html');
 });
 
 io.on('connection', (socket) => {
