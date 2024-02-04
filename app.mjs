42f3921e097c196d42647ed0b3c6aebf845b4aef
StrangePlant05 <iamzeth05@gmail.com>
2024-02-04 16:40:15 +0800

    adad

--- app.mjs
+++ app.mjs
@@ -6,7 +6,7 @@ import { dirname } from 'path';
 
 const app = express();
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 const server = http.createServer(app);
 const io = new SocketIOServer(server);
 
